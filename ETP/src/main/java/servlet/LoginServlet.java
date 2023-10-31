e76ae7bfa80f532371f88d625ec64f3e4e683975
Teppei Kurimatsu <148944070+Solar2210@users.noreply.github.com>
2023-10-31 11:10:12 +0900

    Add files via upload

---
+++ ETP/src/main/java/servlet/LoginServlet.java
@@ -0,0 +1,45 @@
+package servlet;
+
+import java.io.IOException;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String mail = request.getParameter("mail");
+		String pass = request.getParameter("password");
+		
+    	RequestDispatcher dispatcher =
+    			request.getRequestDispatcher("/WEB-INF/jsp/mainMenu.jsp");
+    	dispatcher.forward(request, response);
+	}
+
+}
