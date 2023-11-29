f00236e758d9812f00eabcacdf3cb8d4db8aa5d1
dosu0922 <148944462+dosu0922@users.noreply.github.com>
2023-11-29 11:12:11 +0900

    Create MainMenuServlet.java

---
+++ MainMenuServlet.java
@@ -0,0 +1,29 @@
+package servlet;
+
+import java.io.IOException;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class MainMenuServlet
+ */
+public class MainMenuServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+ 
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		RequestDispatcher dispatcher =
+    			request.getRequestDispatcher("/WEB-INF/jsp/mainMenu.jsp");
+    	dispatcher.forward(request, response);
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+
+}
